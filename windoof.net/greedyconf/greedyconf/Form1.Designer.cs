f88a02fa9fd3f119d8843153502d6dafddcfc7fa
Julian <julian.wingert@subtilitas.de>
2017-12-30 02:18:16 +0100

    initial wondows tests

---
+++ windoof.net/greedyconf/greedyconf/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace greedyconf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbllayout = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbllayout
+            // 
+            this.tbllayout.AutoSize = true;
+            this.tbllayout.ColumnCount = 18;
+            this.tbllayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tbllayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tbllayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tbllayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tbllayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tbllayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tbllayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tbllayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tbllayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tbllayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tbllayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tbllayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tbllayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tbllayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tbllayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tbllayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tbllayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tbllayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tbllayout.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            this.tbllayout.Location = new System.Drawing.Point(3, 3);
+            this.tbllayout.Name = "tbllayout";
+            this.tbllayout.RowCount = 18;
+            this.tbllayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tbllayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tbllayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tbllayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tbllayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tbllayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tbllayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tbllayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tbllayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tbllayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tbllayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tbllayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tbllayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tbllayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tbllayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tbllayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tbllayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tbllayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tbllayout.Size = new System.Drawing.Size(40, 40);
+            this.tbllayout.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.tbllayout);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(998, 810);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 810);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tbllayout;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ windoof.net/greedyconf/greedyconf/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace greedyconf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 16; i++)
+
+                for (int j = 1; j < 17; j++)
+                {
+                    Button tblbtn = new Button();
+                tblbtn.Width = 44;
+                tblbtn.Height = 44;
+                tblbtn.MouseClick += tblbutton_clicked;
+                
+
+                tblbtn.Name = "tblbtn_" + i.ToString() + "_" + j.ToString();
+                tbllayout.Controls.Add(tblbtn, i, j);
+            }
+        }
+
+        private void tblbutton_clicked (object sender, EventArgs e)
+        {
+            Button snd = (Button)sender;
+            snd.BackColor = Color.Black;
+        }
+
+
+    }
+}
