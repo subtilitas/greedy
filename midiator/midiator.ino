8928f40d730ed50c19303a23ed8867e67710faba
Julian <julian.wingert@subtilitas.de>
2018-01-01 15:02:15 +0100

    some working version

--- midiator/midiator.ino
+++ midiator/midiator.ino
@@ -1,6 +1,31 @@
 #include <MIDI.h>
 #include <MsTimer2.h>
+#include "FastLED.h"
 
+FASTLED_USING_NAMESPACE
+
+// FastLED "100-lines-of-code" demo reel, showing just a few 
+// of the kinds of animation patterns you can quickly and easily 
+// compose using FastLED.  
+//
+// This example also shows one easy way to define multiple 
+// animations patterns and have them automatically rotate.
+//
+// -Mark Kriegsman, December 2014
+
+#if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
+	#warning "Requires FastLED 3.1 or later; check github for latest code."
+#endif
+
+#define DATA_PIN    6
+	//#define CLK_PIN   4
+#define LED_TYPE    WS2812B
+#define COLOR_ORDER GRB
+#define NUM_LEDS    256
+	CRGB leds[NUM_LEDS];
+
+#define BRIGHTNESS          255
+#define FRAMES_PER_SECOND  120
 #define ROWS 16
 #define COLS 16
 #define TYPES 3
@@ -19,6 +44,29 @@ void TIRQ_seq_send(void) {
 	if (beat_position >= COLS) {
 		beat_position = 0;
 	}
+	for (int i = 0; i < NUM_LEDS; i++) {
+		if ((i / 16) % 2 == 0) {
+			if (sequencer_data[i / 16][i % 16][velocity] > 0) {
+				leds[i].setRGB(122, 16, 22);
+			}
+			else {
+				leds[i].setRGB(16, 16, 22);
+			}
+		}else {
+			if (sequencer_data[i / 16][15 - (i % 16)][velocity] > 0) {
+				leds[i].setRGB(122, 16, 22);
+			}
+			else {
+				leds[i].setRGB(16, 16, 22);
+			}
+		}
+		//leds[i].setRGB(0,0, 0);
+	}
+	for (int i = 16 * beat_position; i < (16 * beat_position + 16); i++) {
+		leds[i].setRGB(55, 233, 233);
+	}
+
+	FastLED.show();
 
 	for (int row = 0; row < ROWS; row++) {
 		if (sequencer_data[beat_position][row][velocity] == 0) {
@@ -41,6 +89,14 @@ void TIRQ_seq_send(void) {
 
 void setup() {
 	Serial.begin(115200);
+	delay(500); // 3 second delay for recovery
+
+	// tell FastLED about the LED strip configuration
+	FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+	//FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+
+	// set master brightness control
+	FastLED.setBrightness(BRIGHTNESS);
 
 	beat_position = 0;
 	for (int i = 0; i < ROWS; i++) {
